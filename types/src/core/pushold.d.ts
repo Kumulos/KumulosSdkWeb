8e23c5727f61efdf0bea40d8e32d6c7f7eb74526
Christopher Wyllie <c.wyllie@kumulos.com>
2020-02-05 13:38:40 +0000

    Remove extraneous type declarations

--- types/src/core/pushold.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from './index';
-export declare type PushSubscriptionState = 'subscribed' | 'unsubscribed' | 'blocked';
-export declare function registerServiceWorker(path: string): Promise<ServiceWorkerRegistration>;
-export declare function requestNotificationPermission(): Promise<NotificationPermission>;
-export declare function pushRegister(ctx: Context, workerReg: ServiceWorkerRegistration): Promise<void>;
-export declare function requestPermissionAndRegisterForPush(ctx: Context): Promise<PushSubscriptionState>;
-export declare function getCurrentSubscriptionState(ctx: Context): Promise<PushSubscriptionState>;
-export declare function handleAutoResubscription(ctx: Context): Promise<void>;
