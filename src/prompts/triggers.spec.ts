1e1efc86bd18e7e5953b2464aa77f723be77bcad
Christopher Wyllie <c.wyllie@kumulos.com>
2020-09-23 11:44:38 +0100

    Add test cases trapping matching error

---
+++ src/prompts/triggers.spec.ts
@@ -0,0 +1,48 @@
+import { KumulosEvent, PromptConfig } from '../core';
+
+import { triggerMatched } from './triggers';
+
+const cases: [string, string, boolean][] = [
+    ['/', '/', true],
+    ['/', '/foo-bar', false],
+    ['/*', '/foo-bar', true],
+    ['/foo-bar', '/foo-bar', true],
+    ['/foo-bar', '/foo-bar/baz', false],
+    ['/foo-bar/*', '/foo-bar/baz', true],
+    ['/en/*/blog', '/en/2020/blog', true],
+    ['/en/*/blog', '/en/2020/sub', false],
+    ['*/archive', '/2020/archive', true]
+];
+
+test.each(cases)(
+    'propIn filter: %s path: %s matches: %p',
+    (filter, path, expected) => {
+        expect(
+            triggerMatched(newPageViewPrompt(filter), newPageViewEvent(path))
+        ).toBe(expected);
+    }
+);
+
+function newPageViewPrompt(pathFilter: string): PromptConfig {
+    return {
+        position: 'bottom-left',
+        type: 'bell',
+        uuid: 'prompt-1',
+        trigger: {
+            event: 'pageViewed',
+            filters: [['path', 'IN', [pathFilter]]]
+        }
+    };
+}
+
+function newPageViewEvent(path: string): KumulosEvent {
+    return {
+        type: 'pageViewed',
+        timestamp: Date.now(),
+        userId: 'user-1',
+        uuid: `event-${Date.now()}`,
+        data: {
+            path
+        }
+    };
+}
