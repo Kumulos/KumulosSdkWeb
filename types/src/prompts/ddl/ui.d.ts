3575ee278e96b28111dfe8e0bdfae594cc32a12b
Rob Dick <r.dick@kumulos.com>
2021-05-27 14:02:26 +0100

    add defs

---
+++ types/src/prompts/ddl/ui.d.ts
@@ -0,0 +1,18 @@
+import { Component } from 'preact';
+import { DDLPromptConfig } from '../../core';
+interface UiProps {
+    config?: DDLPromptConfig;
+    onBannerConfirm: (config: DDLPromptConfig) => void;
+    onBannerCancelled: (config: DDLPromptConfig) => void;
+}
+export default class Ui extends Component<UiProps, never> {
+    private siteMargin?;
+    private siteTransition?;
+    onDimensions: (_bannerWidth: number, bannerHeight: number) => void;
+    onBannerConfirm: (config: DDLPromptConfig) => void;
+    onBannerCancelled: (config: DDLPromptConfig) => void;
+    resetBodyStyles(): void;
+    renderBanner(): void;
+    render(): import("preact").VNode<any> | null;
+}
+export {};
