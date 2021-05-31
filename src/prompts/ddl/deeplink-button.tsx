c8c62fa37ff33ba93d05762c50e69299c75223d5
Rob Dick <r.dick@kumulos.com>
2021-05-31 10:19:03 +0100

    Swap out clipboard dep for smaller library

--- src/prompts/ddl/deeplink-button.tsx
+++ src/prompts/ddl/deeplink-button.tsx
@@ -1,5 +1,5 @@
 import { Component, h, createRef, RefObject } from 'preact';
-import Clipboard from 'clipboard';
+import copy from 'clipboard-copy';
 
 interface DeeplinkButtonProps {
     style: h.JSX.CSSProperties;
@@ -13,35 +13,25 @@ export default class DeeplinkButton extends Component<
     DeeplinkButtonProps,
     never
 > {
-    private readonly btnRef: RefObject<HTMLButtonElement>;
-    private clipboard?: Clipboard;
-
-    constructor(props: DeeplinkButtonProps) {
-        super(props);
-
-        this.btnRef = createRef<HTMLButtonElement>();
-    }
-
     onTouchEnd = () => {
-        this.clipboard = new Clipboard(this.btnRef.current!);
-        this.clipboard.on('success', e => this.props.onAction());
-    };
+        if (!copy(this.props.linkUrl)) {
+            console.error(
+                'Failed to copy canonical deep link url to clipboard'
+            );
+        }
 
-    componentWillUnmount() {
-        this.clipboard?.destroy();
-    }
+        this.props.onAction();
+    };
 
     render() {
         const { style, class: cssClass, text, linkUrl } = this.props;
 
         return (
             <button
-                ref={this.btnRef}
                 type="button"
                 style={style}
                 class={cssClass}
                 onTouchEnd={this.onTouchEnd}
-                data-clipboard-text={linkUrl}
             >
                 {text}
             </button>
--- types/src/prompts/ddl/deeplink-button.d.ts
+++ types/src/prompts/ddl/deeplink-button.d.ts
@@ -7,11 +7,7 @@ interface DeeplinkButtonProps {
     onAction: () => void;
 }
 export default class DeeplinkButton extends Component<DeeplinkButtonProps, never> {
-    private readonly btnRef;
-    private clipboard?;
-    constructor(props: DeeplinkButtonProps);
     onTouchEnd: () => void;
-    componentWillUnmount(): void;
     render(): h.JSX.Element;
 }
 export {};
