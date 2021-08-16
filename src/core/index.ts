51f191bba6dfa1b356d4382ec1a7318612ee7545
Rob Dick <r.dick@kumulos.com>
2021-08-16 12:45:08 +0100

    Uprev package and bundles

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,7 +2,7 @@ import { authedFetch, cyrb53, uuidv4 } from './utils';
 import { del, get, set } from './storage';
 import { Channel } from './channels';
 
-const SDK_VERSION = '1.9.0';
+const SDK_VERSION = '1.10.0';
 const SDK_TYPE = 10;
 const EVENTS_BASE_URL = 'https://events.kumulos.com';
 export const PUSH_BASE_URL = 'https://push.kumulos.com';
