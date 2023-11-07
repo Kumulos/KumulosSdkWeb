84eeb5a76a5a4ac5e939dc25cc8ba0678faa9256
Konstantin Antipochkin <konstantin_a@optimove.com>
2023-11-07 14:52:06 -0500

    Added push opened and received listeners APIs

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,19 @@ import {
     Configuration,
     Context,
     InstallId,
+    Keys,
+    PlatformConfig,
     PropsObject,
     SDKFeature,
     UserId,
     assertConfigValid,
+    assertKeys,
     associateUser,
     getInstallId,
     getUserId,
     setInstallId,
     trackEvent,
-    trackInstallDetails,
-    PlatformConfig,
-    Keys,
-    assertKeys
+    trackInstallDetails
 } from './core';
 import { WorkerMessageType, isKumulosWorkerMessage } from './worker/messaging';
 import { getBrowserName, isMobile } from './core/utils';
@@ -35,8 +35,6 @@ import RootFrame from './core/root-frame';
 import { loadPlatformAndKeysConfig } from './core/config';
 
 interface KumulosConfig extends Configuration {
-    onPushReceived?: (payload: KumulosPushNotification) => void;
-    onPushOpened?: (payload: KumulosPushNotification) => void;
     originalVisitorId: InstallId;
     customerId?: UserId;
     sdkVersion?: string;
@@ -47,6 +45,9 @@ export default class Kumulos {
     private readonly platformConfig: PlatformConfig;
     private readonly context: Context;
     private readonly rootFrame: RootFrame;
+
+    private onPushReceived?: (payload: KumulosPushNotification) => void;
+    private onPushOpened?: (payload: KumulosPushNotification) => void;
 
     private promptManager?: PromptManager;
     private ddlManager?: DdlManager;
@@ -117,9 +118,6 @@ export default class Kumulos {
             this.rootFrame,
             this.platformConfig.prompts
         );
-
-        this.maybeAddMessageEventListenerToSW();
-        this.maybeFireOpenedHandler();
     }
 
     private async observePermissionStatus() {
@@ -195,8 +193,6 @@ export default class Kumulos {
             serviceWorkerPath: config.serviceWorkerPath,
             autoResubscribe: config.autoResubscribe,
             features: config.features,
-            onPushReceived: config.onPushReceived,
-            onPushOpened: config.onPushOpened,
             originalVisitorId: config.originalVisitorId,
             customerId: config.customerId,
             sdkVersion: config.sdkVersion,
@@ -233,6 +229,16 @@ export default class Kumulos {
         }
     }
 
+    setPushOpenedListener(onPushOpened: (payload: KumulosPushNotification) => void) {
+        this.onPushOpened = onPushOpened;
+        this.maybeFireOpenedHandler();
+    }
+
+    setPushReceivedListener(onPushReceived: (payload: KumulosPushNotification) => void) {
+        this.onPushReceived = onPushReceived;
+        this.maybeAddMessageEventListenerToSW();
+    }
+
     private onWorkerMessage = (e: MessageEvent) => {
         if (e.origin !== location.origin) {
             return;
@@ -245,7 +251,7 @@ export default class Kumulos {
         switch (e.data.type) {
             case WorkerMessageType.KPushReceived: {
                 const push = notificationFromPayload(e.data.data);
-                this.config.onPushReceived?.(push);
+                this.onPushReceived?.(push);
 
                 break;
             }
@@ -260,6 +266,6 @@ export default class Kumulos {
 
         const push = notificationFromPayload(payload);
 
-        this.config.onPushOpened?.(push);
+        this.onPushOpened?.(push);
     }
 }
