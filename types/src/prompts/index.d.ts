2c00c2b457f381fca551be5818edffb5a2d0e7d1
Konstantin Antipochkin <konstantin_a@optimove.com>
2023-11-29 18:03:54 -0500

    Removed dead code

--- types/src/prompts/index.d.ts
+++ types/src/prompts/index.d.ts
@@ -1,6 +1,6 @@
 import { Context, PromptConfigs, PushPromptConfig } from '../core';
 import RootFrame from '../core/root-frame';
-export declare type PromptManagerState = 'loading' | 'ready' | 'requesting' | 'requesting-silent' | 'requesting-silent-dismissed' | 'postaction';
+export declare type PromptManagerState = 'loading' | 'ready' | 'requesting' | 'requesting-silent' | 'requesting-silent-dismissed';
 export declare class PromptManager {
     private readonly context;
     private readonly pushContainer;
